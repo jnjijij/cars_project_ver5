c500c545503c33092e0c98813f114f5af05321b3
Hryhorii_Struk <136825012+jnjijij@users.noreply.github.com>
2024-07-17 10:35:18 +0300

    Delete apps/messages directory

--- apps/messages/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from apps.messages.models import SendMessagesModel
-
-
-class MessageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SendMessagesModel
-        fields = '_all_'
--- apps/messages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import MessageListView
-
-urlpatterns = [
-    path('', MessageListView.as_view())
-]
